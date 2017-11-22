d7276afe4841c23f8d9332791aee0fcb17c84e69
Dominik Fijaś <dominik.fijas@nedap.com>
2017-11-22 11:58:54 +0100

    Use SSL certificate and key while secure connection

--- lib/websocket-client-simple/client.rb
+++ lib/websocket-client-simple/client.rb
@@ -23,6 +23,8 @@ module WebSocket
             ctx = OpenSSL::SSL::SSLContext.new
             ctx.ssl_version = options[:ssl_version] || 'SSLv23'
             ctx.verify_mode = options[:verify_mode] || OpenSSL::SSL::VERIFY_NONE #use VERIFY_PEER for verification
+            ctx.cert = options.fetch(:ssl_certificate)
+            ctx.key = options.fetch(:ssl_private_key)
             cert_store = OpenSSL::X509::Store.new
             cert_store.set_default_paths
             ctx.cert_store = cert_store
